d1867c4514831e98e76e8d12b7816353e8aef9d4
HunCY5 <90296157+HunCY5@users.noreply.github.com>
2026-03-09 23:34:02 +0900

    Initial Commit

---
+++ VibeTrip/VibeTripApp.swift
@@ -0,0 +1,17 @@
+//
+//  VibeTripApp.swift
+//  VibeTrip
+//
+//  Created by CHOI on 2/28/26.
+//
+
+import SwiftUI
+
+@main
+struct VibeTripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
