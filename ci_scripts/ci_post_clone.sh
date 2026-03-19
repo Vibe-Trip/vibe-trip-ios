f986775743e20773430109a72e945c7be9a15b26
HunCY5 <90296157+HunCY5@users.noreply.github.com>
2026-03-19 21:16:21 +0900

    chore: Xcode Cloud CI 환경 설정
    - ci_post_clone.sh 스크립트 추가: GoogleService-Info.plist 자동 생성

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+
+PLIST_PATH="${CI_WORKSPACE}/VibeTrip/GoogleService-Info.plist"
+
+cat > "$PLIST_PATH" <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+    <key>API_KEY</key>
+    <string>${FIREBASE_API_KEY}</string>
+    <key>GCM_SENDER_ID</key>
+    <string>${FIREBASE_GCM_SENDER_ID}</string>
+    <key>PLIST_VERSION</key>
+    <string>1</string>
+    <key>BUNDLE_ID</key>
+    <string>${FIREBASE_BUNDLE_ID}</string>
+    <key>PROJECT_ID</key>
+    <string>${FIREBASE_PROJECT_ID}</string>
+    <key>STORAGE_BUCKET</key>
+    <string>${FIREBASE_STORAGE_BUCKET}</string>
+    <key>IS_ADS_ENABLED</key>
+    <false/>
+    <key>IS_ANALYTICS_ENABLED</key>
+    <false/>
+    <key>IS_APPINVITE_ENABLED</key>
+    <true/>
+    <key>IS_GCM_ENABLED</key>
+    <true/>
+    <key>IS_SIGNIN_ENABLED</key>
+    <true/>
+    <key>GOOGLE_APP_ID</key>
+    <string>${FIREBASE_GOOGLE_APP_ID}</string>
+</dict>
+</plist>
+EOF
+
+echo "GoogleService-Info.plist 생성 완료"
